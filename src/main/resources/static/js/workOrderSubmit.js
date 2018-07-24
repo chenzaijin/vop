a01562f86c2c2f1307e70a5cecca8eb7ce6ead02
chenzaijin <39552218+chenzaijin@users.noreply.github.com>
2018-07-24 10:23:23 +0800

    czj

--- src/main/resources/static/js/workOrderSubmit.js
+++ src/main/resources/static/js/workOrderSubmit.js
@@ -3,8 +3,9 @@
 function WorkOrder() {
 
     var support_deal={};
-    var ={};
-
+    var support_content={};
+    var entiry={};
+    //support_deal
     support_deal.support_id=$("#support_id").val();
     if(support_deal.support_id==null||support_deal.support_id==""){
         alert("工单编号不能为空");
@@ -46,166 +47,172 @@ function WorkOrder() {
         return;
     }
 
-    workOrder.support_level=$("#support_level").val();
-    if(workOrder.support_level==null||workOrder.support_level==""){
+
+
+    //support_content
+    support_content.support_level=$("#support_level").val();
+    if(support_content.support_level==null||support_content.support_level==""){
         alert("工单级别不能为空");
         return;
     }
-    workOrder.issue_province=$("#issue_province").val();
-    if(workOrder.issue_province==null||workOrder.issue_province==""){
+    support_content.issue_province=$("#issue_province").val();
+    if(support_content.issue_province==null||support_content.issue_province==""){
         alert("问题发生省分编码不能为空");
         return;
     }
-    workOrder.issue_city=$("#issue_city").val();
-    if(workOrder.issue_city==null||workOrder.issue_city==""){
+    support_content.issue_city=$("#issue_city").val();
+    if(support_content.issue_city==null||support_content.issue_city==""){
         alert("问题发生地市编码不能为空");
         return;
     }
-    workOrder.phone_number_province=$("#phone_number_province").val();
-    if(workOrder.phone_number_province==null||workOrder.phone_number_province==""){
+    support_content.phone_number_province=$("#phone_number_province").val();
+    if(support_content.phone_number_province==null||support_content.phone_number_province==""){
         alert("用户号码归属省编码");
         return;
     }
-    workOrder.phone_number_city=$("#phone_number_city").val();
-    if(workOrder.phone_number_city==null||workOrder.phone_number_city==""){
+    support_content.phone_number_city=$("#phone_number_city").val();
+    if(support_content.phone_number_city==null||support_content.phone_number_city==""){
         alert("用户号码归属地市编码不能为空");
         return;
     }
-    workOrder.repetition=$("#repetition").val();
-    workOrder.original_support_id=$("#original_support_id").val();
-    workOrder.repetition_description=$("#repetition_description").val();
+    support_content.repetition=$("#repetition").val();
+    support_content.original_support_id=$("#original_support_id").val();
+    support_content.repetition_description=$("#repetition_description").val();
 
-    if(workOrder.repetition==true){
-        if(workOrder.original_support_id==null||workOrder.original_support_id==""){
+    if(support_content.repetition==true){
+        if(support_content.original_support_id==null||support_content.original_support_id==""){
             alert("重复派送工单,原派送工单编号不能为空");
             return;
-        }else if(workOrder.repetition_description==null||workOrder.repetition_description==""){
+        }else if(support_content.repetition_description==null||support_content.repetition_description==""){
             alert("重复派送工单,重复派单说明不能为空");
             return;
         }
     }
 
-    workOrder.present_condition=$("#present_condition").val();
-    if(workOrder.present_condition==null||workOrder.present_condition==""){
+    support_content.present_condition=$("#present_condition").val();
+    if(support_content.present_condition==null||support_content.present_condition==""){
         alert("核查处理情况不能为空");
         return;
     }
-    workOrder.requirements=$("#requirements").val();
-    if(workOrder.requirements==null||workOrder.requirements==""){
+    support_content.requirements=$("#requirements").val();
+    if(support_content.requirements==null||support_content.requirements==""){
         alert("协查处理需求不能为空");
         return;
     }
 
 
-    if(workOrder.support_type=='01') {
+    //entiry
+    if(support_deal.support_type=='01') {
         <!--网络使用类工单实体-->
-        workOrder.network_type = $("#network_type").val();
-        workOrder.signal = $("#signal").val();
-        workOrder.symptom = $("#symptom").val();
-        workOrder.not_calling = $("#not_calling").val();
-        workOrder.issue_description = $("#issue_description").val();
-        if(workOrder.network_type==1){
-            if(workOrder.signal==null||workOrder.signal==""){
+        entiry.network_type = $("#network_type").val();
+        entiry.signal = $("#signal").val();
+        entiry.symptom = $("#symptom").val();
+        entiry.not_calling = $("#not_calling").val();
+        entiry.issue_description = $("#issue_description").val();
+        if(entiry.network_type==1){
+            if(entiry.signal==null||entiry.signal==""){
                 alert("语音业务时,信号情况不能为空");
                 return;
-            }else if(workOrder.symptom==null||workOrder.symptom==""){
+            }else if(entiry.symptom==null||entiry.symptom==""){
                 alert("语音业务时,故障现象不能为空");
                 return;
-            }else if(workOrder.not_calling==null||workOrder.not_calling==""){
+            }else if(entiry.not_calling==null||entiry.not_calling==""){
                 alert("语音业务时,无法主叫不能为空");
                 return;
             }
         }
-    }else if(workOrder.support_type=='03'){
+    }else if(support_deal.support_type=='03'){
         <!--计费类工单实体-->
-        workOrder.cdr_type=$("#cdr_type").val();
-        workOrder.cdr_file=$("#cdr_file").val();
-        workOrder.cdr_number=$("#cdr_number").val();
-        workOrder.billing_issue_description=$("#billing_issue_description").val();
-        if(workOrder.requirements==null||workOrder.requirements==""){
-            alert("协查处理需求不能为空");
-            return;
-        }
-    }else if(workOrder.support_type=='05'){
-        <!--增值业务工单实体-->
-        workOrder.system_type=$("#system_type").val();
-        workOrder.vas_type=$("#vas_type").val();
-        workOrder.vas_description=$("#vas_description").val();
-        workOrder.vas_issue_description=$("#vas_issue_description").val();
-        if(workOrder.vas_issue_description==null||workOrder.vas_issue_description==""){
+        entiry.cdr_type=$("#cdr_type").val();
+        entiry.cdr_file=$("#cdr_file").val();
+        entiry.cdr_number=$("#cdr_number").val();
+        entiry.issue_description=$("#issue_description").val();
+        if(entiry.issue_description==null||entiry.issue_description==""){
             alert("投诉问题描述不能为空");
             return;
         }
-    }else if(workOrder.support_type=='04'){
+    }else if(support_deal.support_type=='05'){
+        <!--增值业务工单实体-->
+        entiry.system_type=$("#system_type").val();
+        entiry.vas_type=$("#vas_type").val();
+        entiry.vas_description=$("#vas_description").val();
+        entiry.vas_issue_description=$("#vas_issue_description").val();
+        if(entiry.issue_description==null||entiry.issue_description==""){
+            alert("投诉问题描述不能为空");
+            return;
+        }
+    }else if(support_deal.support_type=='04'){
         <!--充值类工单实体-->
-        workOrder.paying_arrive=$("#paying_arrive").val();
-        workOrder.recharge_phone_number=$("#recharge_phone_number").val();
-        if(workOrder.recharge_phone_number==null||workOrder.recharge_phone_number==""){
+        entiry.paying_arrive=$("#paying_arrive").val();
+        entiry.recharge_phone_number=$("#recharge_phone_number").val();
+        if(entiry.recharge_phone_number==null||entiry.recharge_phone_number==""){
             alert("充值号码不能为空");
             return;
         }
-        workOrder.card_number=$("#card_number").val();
-        workOrder.recharge_date=$("#recharge_date").val();
-        workOrder.process_serial=$("#process_serial").val();
-        workOrder.payamount=$("#payamount").val();
-        workOrder.recharge_issue_description=$("#recharge_issue_description").val();
-        if(workOrder.recharge_phone_number==null||workOrder.recharge_phone_number==""){
+        entiry.card_number=$("#card_number").val();
+        entiry.recharge_date=$("#recharge_date").val();
+        entiry.process_serial=$("#process_serial").val();
+        entiry.payamount=$("#payamount").val();
+        entiry.issue_description=$("#issue_description").val();
+        if(entiry.issue_description==null||entiry.issue_description==""){
             alert("投诉问题描述不能为空");
             return;
         }
-        if(workOrder.paying_arrive==false){
-            if(workOrder.card_number==null||workOrder.card_number==""){
+        if(entiry.paying_arrive==false){
+            if(entiry.card_number==null||entiry.card_number==""){
                 alert("一卡充卡号不能为空");
                 return;
             }
-            if(workOrder.recharge_date==null||workOrder.recharge_date==""){
+            if(entiry.recharge_date==null||entiry.recharge_date==""){
                 alert("充值时间不能为空");
                 return;
             }
         }else{
-            if(workOrder.process_serial==null||workOrder.process_serial==""){
+            if(entiry.process_serial==null||entiry.process_serial==""){
                 alert("业务流水号不能为空");
                 return;
             }
-            if(workOrder.payamount==null||workOrder.payamount==""){
+            if(entiry.payamount==null||entiry.payamount==""){
                 alert("充值金额不能为空");
                 return;
             }
         }
 
-    }else if(workOrder.support_type=='02'){
+    }else if(support_deal.support_type=='02'){
         <!--开通类投诉实体-->
-        workOrder.api_name=$("#api_name").val();
-        if(workOrder.api_name==null||workOrder.api_name==""){
+        entiry.api_name=$("#api_name").val();
+        if(entiry.api_name==null||entiry.api_name==""){
             alert("API名称不能为空");
             return;
         }
-        workOrder.swift_number=$("#swift_number").val();
-        if(workOrder.swift_number==null||workOrder.swift_number==""){
+        entiry.swift_number=$("#swift_number").val();
+        if(entiry.swift_number==null||entiry.swift_number==""){
             alert("流水号不能为空");
             return;
         }
-        workOrder.order_id=$("#order_id").val();
-        if(workOrder.order_id==null||workOrder.order_id==""){
+        entiry.order_id=$("#order_id").val();
+        if(entiry.order_id==null||entiry.order_id==""){
             alert("定单 ID不能为空");
             return;
         }
-        workOrder.account_issue_description=$("#account_issue_description").val();
-        if(workOrder.account_issue_description==null||workOrder.account_issue_description==""){
+        entiry.issue_description=$("#account_issue_description").val();
+        if(entiry.issue_description==null||entiry.issue_description==""){
             alert("投诉问题描述不能为空");
             return;
         }
-    }else if(workOrder.support_type=='06'){
+    }else if(support_deal.support_type=='06'){
         <!--漏电提醒类工单实体-->
-        workOrder.mishu_issue_description=$("#mishu_issue_description").val();
-        if(workOrder.mishu_issue_description==null||workOrder.mishu_issue_description==""){
+        entiry.issue_description=$("#mishu_issue_description").val();
+        if(entiry.issue_description==null||entiry.issue_description==""){
             alert("投诉问题描述不能为空");
             return;
         }
     }
     <!--附件实体-->
-    workOrder.file_name=$("#file_name").val();
-    workOrder.file_data=$("#file_data").val();
+    //workOrder.file_name=$("#file_name").val();
+    //workOrder.file_data=$("#file_data").val();
+
+    return ""+""+"";
 
 }
 
